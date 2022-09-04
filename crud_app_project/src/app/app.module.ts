22b3fae673fd477a34310b3316b53f175d28dccf
Kumarasamy K <kumarasamyk@salzer-laptop.salzertech.com>
2022-09-04 22:52:35 +0530

    card change

--- crud_app_project/src/app/app.module.ts
+++ crud_app_project/src/app/app.module.ts
@@ -4,21 +4,38 @@ import {  BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MdCardModule} from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import{MatIconModule} from '@angular/material/icon';
+import {MatNativeDateModule} from '@angular/material/core';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //MdButtonModule, MdCheckboxModule,
+   // EmployeesComponent
+
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatNativeDateModule
+    
     
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+ export class AppModule{}
